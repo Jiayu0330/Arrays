6218d1f46d75672c7fb34fd742ad9cb930473fbf
Jiayu0330 <47224691+Jiayu0330@users.noreply.github.com>
2019-02-20 17:16:12 -0500

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,9 @@
+  var animals = ["dragon","sea-otter","bear","bee","unicorn","chicken","deer","dolphin","fox","koala"];
+  document.write("<ul>");
+  document.write("<p>bradshaw chou shabi</p>"); 
+  
+  animals.forEach(myFunction(item){
+    document.write("<li>" + item + "</li>")
+  });
+  
+  document.write("</ul>");
