457b9cc696ed1a6be06461ecf116e224d0096eb6
Jiayu0330 <47224691+Jiayu0330@users.noreply.github.com>
2019-02-20 17:39:18 -0500

    Update array1.js

--- array1.js
+++ array1.js
@@ -1,6 +1,5 @@
   var animals = ["dragon","sea-otter","bear","bee","unicorn","chicken","deer","dolphin","fox","koala"];
   document.write("<ul>");
-  document.write("<p>bradshaw chou shabi</p>"); 
   
   animals.forEach(function(item){
     document.write("<li>" + item + "</li>")
